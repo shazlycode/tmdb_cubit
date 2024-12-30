05497c5c312ea32b7ba5710cd5deaf32c3edcec5
shazlycor <shazlycor@gmail.com>
2024-12-30 12:11:59 +0000

    all

--- lib/screens/movies_screen.dart
+++ lib/screens/movies_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:myapp/Bloc/cubit.dart';
 
 import 'package:myapp/constants/constants.dart';
